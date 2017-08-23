c18ec6e465ed42fcb16c93ee57bcfdb5a4693c63
Alexander Wilson <alexanderwilson@Alexanders-MBP.home>
2017-08-23 23:25:20 +0100

    Trying new approach for js object

--- me.js
+++ me.js
@@ -1,22 +1,22 @@
-    /* JavaScript constant with info about me */
-    const alex = {
-        name: 'Alexander Wilson',
-        dob: '29-01-1996',
-        email: 'acwilson96@protonmail.com',
-        country: 'united_kingdom',
-        city: 'edinburgh',
-        interests: [
-          'food',
-          'travelling',
-          'computer_security',
-          'web_dev',
-          'computer_architecture',
-          'programming',
-          'movies',
-          'skateboarding',
-          'bmxing',
-          'skiing',
-          'dota_2'
-        ]
-      }
+      /* JavaScript constant representing a bit of me */
+      const alex = {
+          name: 'Alexander Wilson',
+          dob: '29-01-1996',
+          email: 'acwilson96@protonmail.com',
+          country: 'united_kingdom',
+          city: 'edinburgh',
+          interests: [
+            'food',
+            'travelling',
+            'computer_security',
+            'web_dev',
+            'computer_architecture',
+            'programming',
+            'movies',
+            'skateboarding',
+            'bmxing',
+            'skiing',
+            'dota_2'
+          ]
+        }
 
