f059cc9ccb7c64247e7e149abc7fe92e083f6f51
Alexander Wilson <acwilson96@gmail.com>
2018-03-17 00:59:46 +0000

    Code cleanup

--- Alex.cpp
+++ Alex.cpp
@@ -4,33 +4,31 @@
       #include <string>
 
       /* c++ Class representing a bit of me */
-      class Alex : public Person {
+      class Alexander : public Person {
+          private:
+              std::string my_email   = "acwilson96@protonmail.com";
           public:
               std::time_t unix_dob;
-              std::string name = "Alexander Wilson";
-              std::string loc  = "Edinburgh, United Kingdom";
-              std::vector<std::string> interests = {
+              std::string name                    = "Alexander Wilson";
+              std::string loc                     = "Edinburgh, United Kingdom";
+              std::vector<std::string> interests  = {
                   "Computer Architecture", "Code Optimisation", "Computer Security",
                   "Software Architecture", "Programming", "Food", "Travelling",
                   "Sudoku", "Skateboarding", "Movies", "Skiing"
               }
 
-              Alex() {
+              Alexander() {
                   unix_dob = 822873600;                                 // Constructed on 29-01-1996
-                  std::cout << "Hey, Welcome to my Webpage!\n";         // Welcome to my webpage.
               }
 
               std::string getContact(bool is_spam) {
                   if (is_spam) return "";
-                  return my_email;  
+                  return my_email;
               }
 
+              // Convert time in seconds to years.
               int getAge() {
-                  std::time_t seconds_alive = std::time(0) - unix_dob;  // Get the number of seconds alive.
-                  return seconds_alive / (60 * 60 * 24 * 365);          // Convert from seconds to years (without leap years).
+                  std::time_t seconds_alive = std::time(0) - unix_dob;
+                  return seconds_alive / (60 * 60 * 24 * 365);
               }
-
-          private:
-              std::string my_email   = "acwilson96@protonmail.com";
-
       }
