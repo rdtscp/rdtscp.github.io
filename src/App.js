ddbf266e83ac87f615c94be4a5a567f9471e6a63
Alexander Wilson <acwilson96@gmail.com>
2018-06-01 12:56:20 +0100

    Single CV Page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route } from "react-router-dom";
 
 import 'bulma/css/bulma.min.css';
 import 'font-awesome/css/font-awesome.min.css';
 import './styles.css';
 
 import About from './Views/About.js';
-import Home from './Views/Home.js';
-import Scrapbook from './Views/Scrapbook.js';
 
 class App extends Component {
 
   render() {
       return (
-        <Router>
-          <div>
-            <Route exact path="/" component={Home} />
-            <Route path="/about" component={About} />
-            <Route path="/scrapbook" component={Scrapbook} />
-          </div>
-        </Router>
+        <About />
       );
   }
 }
