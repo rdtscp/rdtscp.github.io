41b1914a4193315cd2fece1ba9941640968a32fa
Alexander Wilson <acwilson96@gmail.com>
2018-06-01 15:59:46 +0100

    Reset

--- src/Components/index.js
+++
@@ -1,9 +0,0 @@
-// // import Heading from './Heading';
-// // import SubHeading from './Heading';
-import Contents from './Contents';
-import EducationEntry from './EducationEntry';
-import WorkEntry from './WorkEntry';
-import SkillList from './SkillList';
-import Project from './Project';
-
-export { Contents, EducationEntry, WorkEntry, SkillList, Project };
